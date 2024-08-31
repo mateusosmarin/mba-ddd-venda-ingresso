bfeaa278b9a74d21748afbb76a06721d83fe0add
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2024-08-31 08:50:02 -0300

    feat: add customer

---
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -0,0 +1,42 @@
+import { AggregateRoot } from 'src/@core/common/domain/aggregate-root';
+import { Cpf } from 'src/@core/common/domain/value-objects/cpf.vo';
+import { Uuid } from 'src/@core/common/domain/value-objects/uuid.vo';
+
+export class CustomerId extends Uuid {}
+
+type CustomerConstructorProps = {
+  id?: CustomerId | string;
+  cpf: Cpf;
+  name: string;
+};
+
+export class Customer extends AggregateRoot {
+  readonly id: CustomerId;
+  cpf: Cpf;
+  name: string;
+
+  constructor(props: CustomerConstructorProps) {
+    super();
+    this.id =
+      typeof props.id === 'string'
+        ? new CustomerId(props.id)
+        : (props.id ?? new CustomerId());
+    this.cpf = props.cpf;
+    this.name = props.name;
+  }
+
+  static create(command: { name: string; cpf: string }) {
+    return new Customer({
+      name: command.name,
+      cpf: new Cpf(command.cpf),
+    });
+  }
+
+  toJSON() {
+    return {
+      id: this.id.value,
+      cpf: this.cpf,
+      name: this.name,
+    };
+  }
+}
